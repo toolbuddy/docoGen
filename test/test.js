f8e1f4af06aec0397c5fd2665eb1648ab62ab894
kevinbird61 <kevinbird61@gmail.com>
2017-10-10 17:43:13 +0800

    Implement `merge_docogen_ex` for external use.
    * Can using this function to collect all the docogen format files and
    merge all available field in order.
    * For external usage case (e.g. Our docogen HTML generate module)

--- test/test.js
+++ test/test.js
@@ -7,3 +7,9 @@ var t = docogen.generate_latexpdf(__dirname,__dirname+"/dest",{ output: "docogen
 var t1 = docogen.generate_mdpdf(__dirname,__dirname+"/dest",{ output: "docogen-md" },(err,msg)=>{
     console.log(msg);
 });
+
+// extend 
+docogen.merge_docogen_ex(__dirname,{},(err,docObj)=>{
+    // here comes a merged json format from those docogen files under `__dirname`(e.g. your project directory)
+    console.dir(docObj);
+});

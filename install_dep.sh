7016911dc498621318dbbfad380d2c7751ff4909
kevinbird61 <kevinbird61@gmail.com>
2017-11-14 18:23:57 +0800

    Patch for pdfLaTeX dependencies.

--- install_dep.sh
+++ install_dep.sh
@@ -9,6 +9,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
     echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
     sudo apt update && sudo apt install miktex
+	# install tex dependencies
+	sudo apt install texlive texstudio texlive-latex-extra 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     echo "Not Support MacOS script now"
